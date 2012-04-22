192440dd6ae5c8dabda750ac89e67734d350b06f
Eckhart Kőppen <eck@40hz.org>
2012-04-22 11:46:58 +0300

    Changed precompilation for STM32

---
+++ stm32p103/precompiler.s
@@ -0,0 +1,14 @@
+    .include "../precompiler/board.s"
+    .include "stm32p103_words.s"
+
+precompile_words:
+    .include "stm32p103.gen.s"
+    .include "ansi.gen.s"
+    .include "accept.gen.s"
+    .include "editor.gen.s"
+    .include "protothreads.gen.s"
+    .include "quit.gen.s"
+    .word 0xffffffff
+
+    .set last_word, link
+    .set data_start, ram_here
