d2f01bab8d3c736e8e1c881d77a541284e16f4aa
Eckhart Kőppen <eck@40hz.org>
2012-05-08 07:36:33 +0300

    Added KEY?

--- lm3s811/lm3s811_words.s
+++ lm3s811/lm3s811_words.s
@@ -164,9 +164,19 @@
     push {r0}
     NEXT
 
+    defcode "KEY?", KEYQ
+    ldr r0, =addr_SBUF_HEAD
+    ldr r1, [r0]
+    ldr r0, =addr_SBUF_TAIL
+    ldr r0, [r0]
+    subs r0, r1
+    beq 1f
+    movs r0, #0
+    mvns r0, r0
+1:  push {r0}
+    NEXT
+
     defvar "SBUF", SBUF, 16
     defvar "SBUF-HEAD", SBUF_HEAD
     defvar "SBUF-TAIL", SBUF_TAIL
     defvar "IVT", IVT, 48 * 4
-
-
