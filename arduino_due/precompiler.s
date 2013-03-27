0045436b850c1f8a948b776c79b95a01b185fbb2
Eckhart Kőppen <eck@40hz.org>
2013-03-27 22:58:04 +0200

    Added missing precompiler

---
+++ arduino_due/precompiler.s
@@ -0,0 +1,15 @@
+    .include "../precompiler/board.s"
+    .include "arduino_due_words.s"
+
+precompile_words:
+    .include "arduino_due.gen.s"
+    .include "ansi.gen.s"
+    .include "accept.gen.s"
+    @.include "editor.gen.s"
+    @.include "protothreads.gen.s"
+    @.include "multitasking.gen.s"
+    .include "quit.gen.s"
+    .word 0xffffffff
+
+    .set last_word, link
+    .set data_start, ram_here
