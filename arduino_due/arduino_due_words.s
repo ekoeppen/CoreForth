409cfd59c04e288a452d6f13bc5f9be711f0b419
Eckhart Kőppen <eck@40hz.org>
2013-03-31 12:59:51 +0300

    Fix flashing on the Due, improve CMOVE

--- arduino_due/arduino_due_words.s
+++ arduino_due/arduino_due_words.s
@@ -18,10 +18,28 @@
 1:  push {r0}
     NEXT
 
-    .ltorg
+    defcode "FLASH-PAGE", FLASH_PAGE
+    ldr r0, =EEFC0
+    pop {r2}
+1:  ldr r1, [r0, #EEFC_FSR]
+    ands r1, #1
+    beq 1b
+    ldr r1, =0x5A000003
+    lsl r2, #8
+    orrs r1, r2
+    mov r0, #0
+    ldr r2, =0x00100008
+    ldr r2, [r2]
+    adds r2, #1
+    ldr lr, =(cont + 1)
+    mov pc, r2
+cont:
+    NEXT
 
     defvar "SBUF", SBUF, 16
     defvar "SBUF-HEAD", SBUF_HEAD
     defvar "SBUF-TAIL", SBUF_TAIL
     defvar "IVT", IVT, 48 * 4
     defvar "UART0-TASK", UARTZ_TASK
+
+    .ltorg
