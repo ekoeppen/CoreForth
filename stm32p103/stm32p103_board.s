1709ab2a95141ab772e55fc69f9a12a1c13d0a80
Eckhart Kőppen <eck@40hz.org>
2013-12-08 22:11:59 +0200

    Initial USB enabling

--- stm32p103/stm32p103_board.s
+++ stm32p103/stm32p103_board.s
@@ -25,6 +25,72 @@ _start:
     .long 0                           /* Reserved                     */
     .long 0                           /* Reserved                     */
     .long 0                           /* Reserved                     */
+    .long svc_handler + 1             /* SVCall Handler               */
+    .long debugmon_handler + 1        /* Debug Monitor Handler        */
+    .long 0                           /* Reserved                     */
+    .long pendsv_handler + 1          /* PendSV Handler               */
+    .long systick_handler + 1         /* SysTick Handler              */
+    .long wwdg_handler + 1
+    .long pvd_handler + 1
+    .long tamper_handler + 1
+    .long rtc_handler + 1
+    .long flash_handler + 1
+    .long rcc_handler + 1
+    .long exti0_handler + 1
+    .long exti1_handler + 1
+    .long exti2_handler + 1
+    .long exti3_handler + 1
+    .long exti4_handler + 1
+    .long dma1_channel1_handler + 1
+    .long dma1_channel2_handler + 1
+    .long dma1_channel3_handler + 1
+    .long dma1_channel4_handler + 1
+    .long dma1_channel5_handler + 1
+    .long dma1_channel6_handler + 1
+    .long dma1_channel7_handler + 1
+    .long adc1_2_handler + 1
+    .long usb_hp_can_tx_handler + 1
+    .long usb_lp_can_rx_handler + 1
+    .long can_rx1_handler + 1
+    .long can_sce_handler + 1
+    .long exti9_5_handler + 1
+    .long tim1_brk_handler + 1
+    .long tim1_up_handler + 1
+    .long tim1_trg_com_handler + 1
+    .long tim1_cc_handler + 1
+    .long tim2_handler + 1
+    .long tim3_handler + 1
+    .long tim4_handler + 1
+    .long i2c1_ev_handler + 1
+    .long i2c1_er_handler + 1
+    .long i2c2_ev_handler + 1
+    .long i2c2_er_handler + 1
+    .long spi1_handler + 1
+    .long spi2_handler + 1
+    .long usart1_handler + 1
+    .long usart2_handler + 1
+    .long usart3_handler + 1
+    .long exti15_10_handler + 1
+    .long rtcalarm_handler + 1
+    .long usbwakeup_handler + 1
+    .long tim8_brk_handler + 1
+    .long tim8_up_handler + 1
+    .long tim8_trg_com_handler + 1
+    .long tim8_cc_handler + 1
+    .long adc3_handler + 1
+    .long fsmc_handler + 1
+    .long sdio_handler + 1
+    .long tim5_handler + 1
+    .long spi3_handler + 1
+    .long uart4_handler + 1
+    .long uart5_handler + 1
+    .long tim6_handler + 1
+    .long tim7_handler + 1
+    .long dma2_channel1_handler + 1
+    .long dma2_channel2_handler + 1
+    .long dma2_channel3_handler + 1
+    .long dma2_channel4_5_handler + 1
+end_of_irq:
 
     .org 0x150
 
@@ -34,45 +100,89 @@ _start:
 init_board:
     push {lr}
 
-    @ safety delay of 3x8000000 ticks (~ 3 seconds)
-    ldr r0, =8000000
-    bl delay
+    @ switch to 72MHz clock
+    ldr r0, =FPEC
+    mov r1, #0x32
+    str r1, [r0, #FLASH_ACR]
+    ldr r0, =RCC
+    ldr r1, [r0, #RCC_CFGR]
+    ldr r2, =0xffc238ff
+    ands r1, r2
+    ldr r2, =0x001d8400
+    orrs r1, r2
+    str r1, [r0, #RCC_CFGR]
+    ldr r1, =0x00010000
+    str r1, [r0, #RCC_CR]
+    ldr r2, =0x00020000
+1:  ldr r1, [r0, #RCC_CR]
+    ands r1, r2
+    beq 1b
+    ldr r1, =0x01010000
+    str r1, [r0, #RCC_CR]
+    ldr r2, =0x02000000
+2:  ldr r1, [r0, #RCC_CR]
+    ands r1, r2
+    beq 2b
+    ldr r1, [r0, #RCC_CFGR]
+    ldr r2, =0xfffffffc
+    ands r1, r2
+    orrs r1, #0x2
+    str r1, [r0, #RCC_CFGR]
 
     @ reset the interrupt vector table
     ldr r0, =addr_IVT
-    mov r1, #0
-    mov r2, 48
+    movs r1, #0
+    movs r2, #(end_of_irq - _start) / 4
 1:  str r1, [r0], #4
     subs r2, r2, #1
     bgt 1b
 
     @ enable PIC interrupts
-    mov r0, #0
+    movs r0, #0
     msr primask, r0
-    mov r0, #0
+    movs r0, #0
     msr basepri, r0
     ldr r0, =(NVIC + NVIC_SETENA_BASE)
-    mov r1, #0
+    movs r1, #0
     str r1, [r0]
 
-    @ enable clocks on all timers, UARTS, ADC, PWM, SSI and I2C and GPIO ports
+    @ enable clocks
     ldr r0, =RCC
-    ldr r1, =0xffffffff
+
+    @ SPI2 PWR BKP WWD USB I2C2 I2C1 USART USART TIM4 TIM3 TIM2
+    ldr r1, =0x18e64807
     str r1, [r0, #RCC_APB1ENR]
+
+    @ ADC3 SPI1 TIM1 ADC2 ADC1 IOPE IOPD IOPC IOPB IOPA AFIO TIM1
+    ldr r1, =0x00005e7d
     str r1, [r0, #RCC_APB2ENR]
 
     @ enable pins on GPIOA
-    ldr r0, =(GPIOA + GPIO_CRL)
-    ldr r1, =0x44444b44
-    str r1, [r0]
+    ldr r0, =GPIOA
+    ldr r1, =0x444444b4
+    str r1, [r0, #GPIO_CRH]
+
+    @ reset console buffers
+    movs r1, #0
+    ldr r2, =addr_CON_RX_TAIL
+    str r1, [r2]
+    ldr r2, =addr_CON_RX_HEAD
+    str r1, [r2]
+    ldr r2, =addr_CON_TX_TAIL
+    str r1, [r2]
+    ldr r2, =addr_CON_TX_HEAD
+    str r1, [r2]
 
     @ enable UART
+    ldr r0, =(NVIC + NVIC_SETENA_BASE)
+    movs r1, #0x20
+    str r1, [r0, #4]
     ldr r0, =UART1
-    ldr r1, =0x200c
+    ldr r1, =0x206c
     str r1, [r0, #UART_CR1]
 
     @ set UART baud rate
-    ldr r1, =(8000000 / 115200)
+    ldr r1, =(72000000 / 115200)
     str r1, [r0, #UART_BRR]
 
     @ enable SYSTICK
@@ -80,7 +190,7 @@ init_board:
     ldr r1, =0x00ffffff
     str r1, [r0]
     ldr r0, =STCTRL
-    mov r1, #5
+    movs r1, #5
     str r1, [r0]
 
     @ unlock flash controller
@@ -93,27 +203,69 @@ init_board:
     pop {pc}
     .ltorg
 
-readkey:
-    push {r1, r2, r3, lr}
-    ldr r1, =UART2
-    mov r2, #32
-1:  ldr r3, [r1, #UART_SR]
-    and r3, r2
-    cmp r3, r2
-    bne 1b
+readkey_polled:
+    push {r1, r2}
+    ldr r1, =UART1
+1:  ldr r2, [r1, #UART_SR]
+    ands r2, #32
+    beq 1b
     ldrb r0, [r1, #UART_DR]
+    pop {r1, r2}
+    bx lr
+
+putchar_polled:
+    push {r1, r2}
+    ldr r1, =UART1
+1:  ldr r2, [r1, #UART_SR]
+    ands r2, #0x80
+    beq 1b
+    str r0, [r1, #UART_DR]
+    pop {r1, r2}
+    bx lr
+
+readkey:
+readkey_int:
+    push {r1, r2, r3, lr}
+    ldr r1, =addr_CON_RX_TAIL
+    ldr r3, [r1]
+2:  ldr r2, =addr_CON_RX_HEAD
+    ldr r2, [r2]
+    cmp r2, r3
+    bne 1f
+    wfi
+    b 2b
+1:  ldr r0, =addr_CON_RX
+    ldrb r0, [r0, r3]
+    adds r3, #1
+    ands r3, #0x3f
+    str r3, [r1]
     pop {r1, r2, r3, pc}
 
 putchar:
+putchar_int:
     push {r1, r2, r3, lr}
-    mov r2, #0x80
-    ldr r3, =UART2
-1:  ldr r1, [r3, #UART_SR]
-    and r1, r2
-    cmp r1, r2
-    bne 1b
-    str r0, [r3, #UART_DR]
-    pop {r1, r2, r3, pc}
+    ldr r1, =addr_CON_TX_HEAD
+    ldr r2, [r1]
+    ldr r3, =addr_CON_TX_TAIL
+    ldr r3, [r3]
+    cmp r2, r3
+    bne 2f
+    bl putchar_polled
+    b 4f
+2:  ldr r3, =addr_CON_TX_HEAD
+    adds r2, #1
+    ands r2, #0x3f
+3:  ldr r3, [r3]
+    cmp r2, r3
+    bne 1f
+    wfi
+    b 3b
+1:  str r2, [r1]
+    subs r2, #1
+    ands r2, #0x3f
+    ldr r3, =addr_CON_TX
+    strb r0, [r3, r2]
+4:  pop {r1, r2, r3, pc}
 
 @ ---------------------------------------------------------------------
 @ -- IRQ handlers -----------------------------------------------------
@@ -136,9 +288,9 @@ generic_forth_handler:
     ldr r6, =irq_stack_top
     mov r7, r0
     ldr r0, [r7]
-    add r7, r7, #4
+    adds r7, #4
     ldr r1, [r0]
-    add r1, r1, #1
+    adds r1, #1
     bx r1
 1:  bx lr
 
@@ -146,7 +298,10 @@ nmi_handler:
     b .
 
 hardfault_handler:
-    mrs r0, psp
+    tst lr, #4
+    ite eq
+    mrseq r0, msp
+    mrsne r0, psp
     b .
 
 memmanage_handler:
@@ -167,33 +322,104 @@ debugmon_handler:
 pendsv_handler:
     b .
 
-systick_handler:
-    b .
+usart1_handler:
+    ldr r1, =(UART1 + UART_SR)
+    ldr r1, [r1]
+    tst r1, #0x20
+    beq 1f
+    ldr r0, =addr_CON_RX
+    ldr r1, =addr_CON_RX_HEAD
+    ldr r2, [r1]
+    ldr r3, =(UART1 + UART_DR)
+    ldrb r3, [r3]
+    strb r3, [r0, r2]
+    adds r2, #1
+    ands r2, #0x3f
+    str r2, [r1]
+    b 2f
+1:  ldr r1, =(UART1 + UART_SR)
+    ldr r1, [r1]
+    tst r1, #0x60
+    beq 2f
+    ldr r1, =addr_CON_TX_HEAD
+    ldr r1, [r1]
+    ldr r2, =addr_CON_TX_TAIL
+    ldr r3, [r2]
+    cmp r1, r3
+    beq 2f
+    ldr r0, =addr_CON_TX
+    ldrb r1, [r0, r3]
+    ldr r0, =(UART1 + UART_DR)
+    strb r1, [r0]
+    adds r3, #1
+    ands r3, #0x3f
+    str r3, [r2]
+2:  movs r0, #0
+    ldr r1, =(UART1 + UART_SR)
+    str r0, [r1]
+    bx lr
+    .align 2,0
 
-gpioa_handler:
-gpiob_handler:
-gpioc_handler:
-gpiod_handler:
-gpioe_handler:
-uart0_handler:
-uart1_handler:
-ssi_handler:
-i2c_handler:
-pwm0_handler:
-pwm1_handler:
-pwm2_handler:
-adcseq0_handler:
-adcseq1_handler:
-adcseq2_handler:
-adcseq3_handler:
-watchdog_handler:
-timer0a_handler:
-timer0b_handler:
-timer1a_handler:
-timer1b_handler:
-timer2a_handler:
-timer2b_handler:
-adcomp_handler:
+systick_handler:
+adc1_2_handler:
+adc3_handler:
+can_rx1_handler:
+can_sce_handler:
+dma1_channel1_handler:
+dma1_channel2_handler:
+dma1_channel3_handler:
+dma1_channel4_handler:
+dma1_channel5_handler:
+dma1_channel6_handler:
+dma1_channel7_handler:
+dma2_channel1_handler:
+dma2_channel2_handler:
+dma2_channel3_handler:
+dma2_channel4_5_handler:
+exti0_handler:
+exti15_10_handler:
+exti1_handler:
+exti2_handler:
+exti3_handler:
+exti4_handler:
+exti9_5_handler:
+flash_handler:
+fsmc_handler:
+i2c1_er_handler:
+i2c1_ev_handler:
+i2c2_er_handler:
+i2c2_ev_handler:
+pvd_handler:
+rcc_handler:
+rtc_handler:
+rtcalarm_handler:
+sdio_handler:
+spi1_handler:
+spi2_handler:
+spi3_handler:
+tamper_handler:
+tim1_brk_handler:
+tim1_cc_handler:
+tim1_trg_com_handler:
+tim1_up_handler:
+tim2_handler:
+tim3_handler:
+tim4_handler:
+tim5_handler:
+tim6_handler:
+tim7_handler:
+tim8_brk_handler:
+tim8_cc_handler:
+tim8_trg_com_handler:
+tim8_up_handler:
+uart4_handler:
+uart5_handler:
+usart2_handler:
+usart3_handler:
+usb_hp_can_tx_handler:
+usb_lp_can_rx_handler:
+usbwakeup_handler:
+wwdg_handler:
     b generic_forth_handler
 
 @ ---------------------------------------------------------------------
--- stm32p103/stm32p103_words.s
+++ stm32p103/stm32p103_words.s
@@ -1,12 +1,41 @@
     .ltorg
 
+sysclock:
+    push {r4, r5}
+    ldr r0, =8000000
+    ldr r4, =RCC
+    ldr r4, [r4, #RCC_CFGR]
+    mov r5, r4
+    ands r5, #0x0c
+    beq 1f
+    cmp r5, #0x04
+    beq 1f
+    lsrs r4, #16
+    ands r4, #0x3f
+    mov r5, r4
+    ands r5, #0x01
+    beq 2f
+    ands r5, #0x02
+    beq 3f
+2:  lsrs r0, #1
+3:  lsrs r4, #2
+    adds r4, #2
+    mul r0, r0, r4
+1:  pop {r4, r5}
+    bx lr
+
+    defcode "SYSCLOCK", SYSCLOCK
+    bl sysclock
+    push {r0}
+    NEXT
+
     defcode "RETI", RETI
     pop {r4 - r12, pc}
 
     defword ";I", SEMICOLONI, F_IMMED
     .word LIT, RETI, COMMAXT, REVEAL, LBRACKET, EXIT
 
-    defvar "IVT", IVT, 75 * 4
+    defvar "IVT", IVT, (end_of_irq - _start)
 
     defcode "KEY?", KEYQ
     mov r2, #0
@@ -18,10 +47,61 @@
 1:  push {r2}
     NEXT
 
-    defvar "SBUF", SBUF, 16
-    defvar "SBUF-HEAD", SBUF_HEAD
-    defvar "SBUF-TAIL", SBUF_TAIL
-    defvar "IVT", IVT, 48 * 4
+    defcode "ERASE-PAGE", ERASE_PAGE
+    pop {r1}
+    ldr r0, =FPEC
+    mov r2, #0x2
+    str r2, [r0, #FLASH_CR]
+    str r1, [r0, #FLASH_AR]
+    ldr r2, [r0, #FLASH_CR]
+    orrs r2, #0x40
+    str r2, [r0, #FLASH_CR]
+1:  ldr r2, [r0, #FLASH_SR]
+    ands r2, #0x1
+    bne 1b
+    NEXT
+
+    defcode "FLASH-PAGE", FLASH_PAGE
+    pop {r2}
+    pop {r3}
+    mov r4, #0x400
+    ldr r0, =FPEC
+1:  mov r1, #0x1
+    str r1, [r0, #FLASH_CR]
+    ldrh r1, [r3]
+    strh r1, [r2]
+2:  ldr r1, [r0, #FLASH_SR]
+    ands r1, #0x1
+    bne 2b
+    adds r2, #2
+    adds r3, #2
+    subs r4, #2
+    bne 1b
+    NEXT
+
+    defcode "CON-RX!", CON_RXSTORE
+    ldr r0, =addr_CON_RX
+    ldr r1, =addr_CON_RX_HEAD
+con_store:
+    pop {r3}
+    ldr r2, [r1]
+    strb r3, [r0, r2]
+    adds r2, #1
+    ands r2, #0x3f
+    str r2, [r1]
+    NEXT
+
+    defcode "CON-TX!", CON_TXSTORE
+    ldr r0, =addr_CON_TX
+    ldr r1, =addr_CON_TX_HEAD
+    b con_store
+
+    defvar "CON-RX-TAIL", CON_RX_TAIL
+    defvar "CON-RX-HEAD", CON_RX_HEAD
+    defvar "CON-RX", CON_RX, 64
+    defvar "CON-TX-TAIL", CON_TX_TAIL
+    defvar "CON-TX-HEAD", CON_TX_HEAD
+    defvar "CON-TX", CON_TX, 64
     defvar "UART0-TASK", UARTZ_TASK
 
     .ltorg
