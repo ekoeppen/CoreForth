0ee7b3c071d380b88804bf8d02bbfe1eca8f260d
Eckhart Kőppen <eck@40hz.org>
2014-02-13 21:23:16 +0200

    Completing more word sets

--- thumbolator/thumbolator_words.s
+++ thumbolator/thumbolator_words.s
@@ -6,12 +6,31 @@
 
     .org 0x400
     .set ram_start, 0x20000000
+    .set eval_words, 0x00010000
 
     .include "CoreForth.s"
 
+    defcode "RETI", RETI
+    pop {r4}
+    mov r12, r4
+    pop {r4}
+    mov r11, r4
+    pop {r4}
+    mov r10, r4
+    pop {r4}
+    mov r9, r4
+    pop {r4}
+    mov r8, r4
+    pop {r4 - r7, pc}
+
+    defword ";I", SEMICOLONI, F_IMMED
+    .word LIT, RETI, COMMAXT, REVEAL, LBRACKET, EXIT
+
+    defvar "IVT", IVT, 0x140
+
     defword "COLD", COLD
     .word EMULATIONQ, QBRANCH, 1f - .
-    .word ROM, LIT, eval_words, EVALUATE
+    .word ROM, LIT, eval_words, DUP, LIT, 40, TYPE, CR, EVALUATE
     .word HERE, LIT, init_here, STORE
     .word RAM_DP, FETCH, LIT, init_data_start, STORE
     .word LATEST, FETCH, LIT, init_last_word, STORE
@@ -19,17 +38,8 @@
 1:  .word RAM, LIT, startup_words, EVALUATE
 startup_words:
     .include "thumbolator_ram.gen.s"
-    .word 0xffffffff
+    .align 2,0
 
     .set last_word, link
     .set data_start, ram_here
     .set here, .
-
-    .org . + 0x10000
-eval_words:
-    @ .include "thumbolator.gen.s"
-    .include "ansi.gen.s"
-    .include "quit.gen.s"
-    .byte 0
-    .align 2,0
-
